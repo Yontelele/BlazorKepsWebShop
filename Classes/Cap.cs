78b50145341cb0d451408a2bf72c6ef795aa4924
Jonathan Larsson <jonathan.larsson@iths.se>
2024-03-10 03:04:54 +0100

    Add project files.

---
+++ Classes/Cap.cs
@@ -0,0 +1,19 @@
+﻿namespace LaborationBlazorKepsWebShop.Classes;
+
+public class Cap
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Color { get; set; }
+    public int Price { get; set; }
+    public string ImageUrl { get; set; }
+
+    public Cap(int id, string name, string color, int price, string imageurl)
+    {
+        Id = id;
+        Name = name;
+        Color = color;
+        Price = price;
+        ImageUrl = imageurl;
+    }
+}
